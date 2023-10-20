e7c98c2ea99686db38160e8d06a1baeea856f655
BhaskarKr777 <143020163+BhaskarKr777@users.noreply.github.com>
2023-10-20 22:03:26 +0530

    Update divisibility.py

--- divisibility.py
+++ divisibility.py
@@ -1,6 +1,8 @@
-#for divisibility
-num = int(input("Enter a number: "))
-if num % 2 == 0 and num % 3 == 0:
-    print(f"{num} is divisible by both 2 and 3.")
+num = int(input("Enter number: "))
+if num % 2 == 0:
+    if num % 3 == 0:
+        print(f"{num} is divisible by 2 and 3.")
+    else:
+        print(f"{num} is divisible by 2 but not by 3.")
 else:
-    print(f"{num} is not divisible by both 2 and 3.")
+    print(f"{num} is not divisible by 2 and 3.")
