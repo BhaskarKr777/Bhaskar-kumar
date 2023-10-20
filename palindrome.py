c289822fb1a868b1ada2b4392b420056715fa42e
BhaskarKr777 <143020163+BhaskarKr777@users.noreply.github.com>
2023-10-20 21:55:16 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,10 @@
-#for palindrome number
 def is_palindrome(num):
 
     num_str = str(num)
 
     return num_str == num_str[::-1]
 
-num = int(input("Enter an integer: "))
+num = int(input("Enter number: "))
 
 if is_palindrome(num):
     print(f"{num} is a palindrome.")
